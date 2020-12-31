6a8c7455a2d9d894ea14a96f3fc1229079c560b8
Javier De La Fuente Sales <javierdelafuentesales@gmail.com>
2020-12-31 15:14:48 +0100

    feat: added basic login

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,13 @@
 import { User } from "../entities/User";
-import { Arg, Field, InputType, Mutation, Resolver } from "type-graphql";
-import { hash } from "argon2";
+import {
+  Arg,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
+import { hash, verify } from "argon2";
 
 @InputType()
 class UsernamePasswordInput {
@@ -10,13 +17,90 @@ class UsernamePasswordInput {
   @Field()
   password: string;
 }
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
-  async register(@Arg("options") options: UsernamePasswordInput) {
-    return User.create({
-      username: options.username,
-      password: await hash(options.password),
-    }).save();
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "lenght must be greater than 2",
+          },
+        ],
+      };
+    }
+
+    /* TODO - add better password validation */
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "lenght must be greater than 3",
+          },
+        ],
+      };
+    }
+
+    try {
+      const user = await User.create({
+        username: options.username,
+        password: await hash(options.password),
+      }).save();
+
+      return { user };
+    } catch (err) {
+      //duplicate username error
+      if (err.code === "23505") {
+        return {
+          errors: [{ field: "username", message: "Username already exists" }],
+        };
+      }
+
+      throw err;
+    }
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput
+  ): Promise<UserResponse> {
+    const user = await User.findOne({ username: options.username });
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "User does not exist" }],
+      };
+    }
+
+    if (!(await verify(user.password, options.password))) {
+      return {
+        errors: [{ field: "password", message: "Credentials are not valid" }],
+      };
+    }
+
+    return { user };
   }
 }
